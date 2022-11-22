620ff80608e37b3aadecdb808ba860d7cab88756
Attila Zsíros <attila.zsiros@outlook.sk>
2022-11-22 10:27:07 +0100

    init

---
+++ adaptive-lifting-scheme.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pywt.data
+from numpy import fft
+from skimage import io as skio
+
+img = skio.imread('cameraman.tif')
+#img = pywt.data.camera()
+
+coeffs = pywt.dwt2(img, 'coif1')
+LL, (LH, HL, HH) = coeffs
+
+fig = plt.figure(figsize=(8,8))
+
+gs = fig.add_gridspec(nrows=2, ncols=2, hspace=0.0, wspace=0.0)
+
+gs01 = gs[0, 1].subgridspec(nrows=2, ncols=2, hspace=0.0, wspace=0.0)
+for i, a in enumerate([LL, LH, HL, HH]):
+    ax = fig.add_subplot(gs01[i])
+    ax.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    
+
+for i, a in enumerate([LL, LH, HL, HH]):
+    if i == 1:
+        continue
+    ax = fig.add_subplot(gs[i])
+    ax.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+fig.tight_layout()
+plt.show()
