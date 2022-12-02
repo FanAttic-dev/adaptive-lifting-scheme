7e5a9109572382e3622ec2cdd081d5150b4b3a04
Attila Zsíros <attila.zsiros@outlook.sk>
2022-12-02 09:30:49 +0100

    lifting scheme 2d var complete

--- lifting_scheme_2d_var.py
+++ lifting_scheme_2d_var.py
@@ -2,6 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io as skio
 
+class DWTLevel:
+    def __init__(self, img):
+        LL, (LH, HL, HH) = lifting_scheme_haar2(img)
+        self.coeffs = [LL, LH, HL, HH]
+        
+    def add_level(self, idx):
+        """ Creates a new dwt level at position `idx` and stores it in `self.coeffs`. """
+        coeff = self.coeffs[idx]
+        
+        if len(coeff) <= 1:
+            return None
+        
+        level = DWTLevel(coeff)
+        self.coeffs[idx] = level
+        
+        return level
+    
+    def get_next_level(self):
+        """ Returns the next dwt level if exists. If the current dwt level is a leaf, returns None """
+        next_level_list = [coeff for coeff in self.coeffs if isinstance(coeff, DWTLevel)]
+        if len(next_level_list) == 0:
+            return None
+        return next_level_list[0]
+        
+    def get_max_var_coeff_idx(self):
+        """ Returns the index of the coefficient with the maximal variance. """
+        return np.argmax(list(map(lambda coeff: np.var(coeff), self.coeffs)))
+    
+
 def lifting_scheme_haar(f):
     # split
     L, H = f[::2], f[1::2]
@@ -13,6 +42,7 @@ def lifting_scheme_haar(f):
     L += np.floor(H / 2).astype(dtype)
     
     return L, H
+
 
 def lifting_scheme_haar2(img):
     f = img.flatten(order='C').astype(dtype)
@@ -33,17 +63,26 @@ def lifting_scheme_haar2(img):
     
     return LL, (LH, HL, HH)
 
+
 def lifting_scheme_haar_inverse(L, H):
+    # update
     L -= np.floor(H / 2).astype(dtype)
+    
+    # predict
     H += np.floor(L).astype(dtype)
     
+    # merge
     res = np.zeros(len(L) + len(H), dtype=dtype)
     res[::2] = L
     res[1::2] = H
     return res
 
+
 def lifting_scheme_haar2_inverse(coeffs):
-    LL, (LH, HL, HH) = coeffs
+    if isinstance(coeffs, list):
+        LL, LH, HL, HH = coeffs
+    elif isinstance(coeffs, tuple):
+        LL, (LH, HL, HH) = coeffs        
     
     new_shape = LL.shape[0] * 2, LL.shape[1]
     
@@ -55,8 +94,6 @@ def lifting_scheme_haar2_inverse(coeffs):
     L = lifting_scheme_haar_inverse(LL, LH)
     H = lifting_scheme_haar_inverse(HL, HH)
     
-    #return np.reshape(L, new_shape, order='F')
-    
     L = np.reshape(L, new_shape, order='F').flatten(order='C').astype(dtype)
     H = np.reshape(H, new_shape, order='F').flatten(order='C').astype(dtype)
     
@@ -66,45 +103,9 @@ def lifting_scheme_haar2_inverse(coeffs):
     img_rec = np.reshape(f_rec, new_shape, order='C')
     
     return img_rec
-    
-    
-def lifting_scheme_haar_dec(img):
-    f = img.flatten().astype(dtype)
-    
-    L, H = lifting_scheme_haar(f)
-    
-    H_coeffs = [H]
-    while len(L) > 1:
-        L, H = lifting_scheme_haar(L)
-        H_coeffs.append(H)
-    
-    return L, H_coeffs
-
-class DWTLevel:
-    def __init__(self, img):
-        LL, (LH, HL, HH) = lifting_scheme_haar2(img)
-        self.coeffs = [LL, LH, HL, HH]
-        self.next_level_idx = None
-        
-    def add_level(self, idx):
-        coeff = self.coeffs[idx]
-        
-        if len(coeff) <= 1:
-            return None
-        
-        level = DWTLevel(coeff)
-        self.coeffs[idx] = level
-        self.next_level_idx = idx
-        return level
-        
-    def get_next_level_idx(self):
-        return self.next_level_idx
-        
-    def get_max_var_coeff_idx(self):
-        return np.argmax(list(map(lambda coeff: np.var(coeff), self.coeffs)))
         
 
-def lifting_scheme_haar_dec2(img, max_level=np.inf):
+def lifting_scheme_haar2_dec(img, max_level=np.inf):
     root = DWTLevel(img)
     
     dwt_level = root
@@ -117,33 +118,46 @@ def lifting_scheme_haar_dec2(img, max_level=np.inf):
     return root
 
 
+def lifting_scheme_haar2_rec(level):
+    next_level = level.get_next_level()
+    if next_level is None:
+        return lifting_scheme_haar2_inverse(level.coeffs)
+    
+    dwt_level_rec = lifting_scheme_haar2_rec(next_level)
+    coeffs = [dwt_level_rec if isinstance(coeff, DWTLevel) else coeff for coeff in level.coeffs]
+    return lifting_scheme_haar2_inverse(coeffs)
+
+
 def visualize_dec(root_level, max_level=None):
     fig = plt.figure(figsize=(8,8))
     gs = fig.add_gridspec(nrows=2, ncols=2, hspace=0.0, wspace=0.0)
     
     level = root_level
     while True:
-        next_level_idx = level.get_next_level_idx()
-        
-        for i in range(len(level.coeffs)):
-            if i == next_level_idx:
+        next_level = None
+        for i, coeff in enumerate(level.coeffs):
+            if isinstance(coeff, DWTLevel):
+                next_level = coeff
                 continue
+            
             ax = fig.add_subplot(gs[i])
-            ax.imshow(level.coeffs[i], interpolation="nearest", cmap=plt.cm.gray)
+            ax.imshow(coeff, interpolation="nearest", cmap=plt.cm.gray)
             ax.set_xticks([])
             ax.set_yticks([])
         
-        if next_level_idx is None:
+        if next_level is None:
             break
         else:
-            level = level.coeffs[next_level_idx]
-            gs = gs[next_level_idx].subgridspec(nrows=2, ncols=2, hspace=0.0, wspace=0.0)
+            gs = gs[level.coeffs.index(next_level)].subgridspec(nrows=2, ncols=2, hspace=0.0, wspace=0.0)
+            level = next_level
         
     fig.tight_layout()
     plt.show()
 
 
 dtype = np.int16
-img = skio.imread('cameraman.tif')    
-root_level = lifting_scheme_haar_dec2(img, max_level=3)
+img = skio.imread('cameraman.tif')
+root_level = lifting_scheme_haar2_dec(img, max_level=3)
+img_rec = lifting_scheme_haar2_rec(root_level)
+#plt.imshow(img_rec, cmap='gray')
 visualize_dec(root_level)
